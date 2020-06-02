3ecb8d8d8c18f48353c012900ea2a8fd7ab95b5c
elgndy <elgndy.50101@gmail.com>
2020-06-02 15:55:46 +0200

    Updates

---
+++ precache-manifest.d185bb8700252b3d61993d8da1567c34.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5daa8be56e794f9998f5ef2158b50b1",
+    "url": "/elgndy/beach/index.html"
+  },
+  {
+    "revision": "4db4ac3f812383fe5cfd",
+    "url": "/elgndy/beach/static/css/main.45ed9aa4.chunk.css"
+  },
+  {
+    "revision": "daa0abffd74692a1110e",
+    "url": "/elgndy/beach/static/js/2.d0615616.chunk.js"
+  },
+  {
+    "revision": "4db4ac3f812383fe5cfd",
+    "url": "/elgndy/beach/static/js/main.ebe2122d.chunk.js"
+  },
+  {
+    "revision": "f95cc1daa0c79651c0ae",
+    "url": "/elgndy/beach/static/js/runtime~main.40bd46bd.js"
+  },
+  {
+    "revision": "f83203e4bf5c35c3f4c0dfdfcd3a508c",
+    "url": "/elgndy/beach/static/media/default-bac.f83203e4.png"
+  },
+  {
+    "revision": "2a7c5842778815d88640c3048abe7c5c",
+    "url": "/elgndy/beach/static/media/details1.2a7c5842.png"
+  },
+  {
+    "revision": "b974031f19db8da31414c48cd816002e",
+    "url": "/elgndy/beach/static/media/details2.b974031f.png"
+  },
+  {
+    "revision": "fc7ca9a61c1561bdf428fb8e5596d0f5",
+    "url": "/elgndy/beach/static/media/room1.fc7ca9a6.png"
+  },
+  {
+    "revision": "f2c105537c6bdfc72df23970344afa29",
+    "url": "/elgndy/beach/static/media/room10.f2c10553.png"
+  },
+  {
+    "revision": "7a9deeb3b34dd29b7251e148c080cadc",
+    "url": "/elgndy/beach/static/media/room13.7a9deeb3.png"
+  },
+  {
+    "revision": "6d09089ec11109747f57388662973d3e",
+    "url": "/elgndy/beach/static/media/room14.6d09089e.png"
+  },
+  {
+    "revision": "bf84ca501f457afba8fb235853a8ae4f",
+    "url": "/elgndy/beach/static/media/room15.bf84ca50.png"
+  },
+  {
+    "revision": "1d750cc716f0f682ceb73ce4164a3c6d",
+    "url": "/elgndy/beach/static/media/room16.1d750cc7.png"
+  },
+  {
+    "revision": "51e2f83e89f26a73716e886e2d9b5c2c",
+    "url": "/elgndy/beach/static/media/room2.51e2f83e.png"
+  },
+  {
+    "revision": "7099609034c0d2e9d9b87fd585c255f8",
+    "url": "/elgndy/beach/static/media/room3.70996090.png"
+  },
+  {
+    "revision": "1dd1fab72ed45de29fe07bf280e2d928",
+    "url": "/elgndy/beach/static/media/room4.1dd1fab7.png"
+  },
+  {
+    "revision": "2df6c5bc5c6c207ea1a0df580395987b",
+    "url": "/elgndy/beach/static/media/room5.2df6c5bc.png"
+  },
+  {
+    "revision": "ae6504ace19b935aa22c4542fc82e781",
+    "url": "/elgndy/beach/static/media/room7.ae6504ac.png"
+  },
+  {
+    "revision": "42cfdaed006acc0cf84d499c9643a89c",
+    "url": "/elgndy/beach/static/media/room8.42cfdaed.png"
+  },
+  {
+    "revision": "eca1805dfdd2dcb56258912730bba09b",
+    "url": "/elgndy/beach/static/media/room9.eca1805d.png"
+  },
+  {
+    "revision": "bf9ddf1afb198b2c6c478c525a97b20e",
+    "url": "/elgndy/beach/static/media/ved.bf9ddf1a.mp4"
+  }
+]);
